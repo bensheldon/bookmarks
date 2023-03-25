f0074714ecb07de02e6d86697187cd7ec9b77f50
Ben Sheldon [he/him] <bensheldon@gmail.com>
2023-03-25 09:34:02 -0700

    Add published key

--- script/lib/bookmark.rb
+++ script/lib/bookmark.rb
@@ -1,13 +1,14 @@
 require 'yaml'
 require 'securerandom'
 require 'time'
+
 class Bookmark
-  attr_accessor :url, :title, :tags, :notes, :created_at, :id
+  attr_accessor :url, :title, :tags, :notes, :created_at, :published, :id
 
   def self.load_all
     Dir.glob("_bookmarks/*.md").map do |path|
       load(path)
-    end
+    end.sort_by(&:created_at)
   end
 
   def self.load(path)
@@ -24,16 +25,18 @@ class Bookmark
       tags: frontmatter["tags"],
       notes: body,
       created_at: Time.parse(frontmatter["created_at"]),
+      published: frontmatter["published"],
       id: id,
     )
   end
 
-  def initialize(url:, title: nil, tags: nil, notes: nil, created_at: nil, id: nil)
+  def initialize(url:, title: nil, tags: nil, notes: nil, created_at: nil, published: false, id: nil)
     @url = url
     @title = title
     @tags = tags
     @notes = notes
     @created_at = created_at || Time.new
+    @published = published
     @id = id || SecureRandom.uuid
   end
 
@@ -53,6 +56,7 @@ class Bookmark
     frontmatter = {
       url: url,
       created_at: Time.new.strftime('%Y-%m-%d %H:%M %Z'),
+      published: published,
       title: title,
       tags: tags,
     }.transform_keys(&:to_s)
